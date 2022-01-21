9f5b68b0f9dd228b96ddf3230939624d67ac31b5
Shawn Rodgers <shawnrodgers266@gmail.com>
2022-01-20 21:02:26 -0500

    venv setup, created exec script

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+
+    Wordle Solver.
+
+'''
+
+from termcolor import colored
+from os import system
+
+def main():
+
+    print('Test')
+
+if __name__ == '__main__':
+    system('cls')
+    print(colored('ERROR: Please run from run.py', 'red'))
---
+++ run.py
@@ -0,0 +1,10 @@
+'''
+
+    Exec file for wordle solver.
+
+'''
+
+from main import main
+
+if __name__ == '__main__':
+    main()
